13e4cd32329426031441873410e693c97e749f48
hmc-snake-s18 <35538542+hmc-snake-s18@users.noreply.github.com>
2019-02-02 17:22:46 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1sgxY5bVaP3NUTS-rC9fSNVL6Ej5RVL0W'
+export_file_url = 'https://niio3w9y.gradient.paperspace.com/files/course-v3/nbs/dl1/data/Horsezebradonkey/export.pkl?download=1'
 export_file_name = 'export.pkl'
 
 classes = ['Zebra', 'Horse', 'Donkey']
