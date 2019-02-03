a1ef14734ecf37e2a9844546211fc7f6a4054c75
hmc-snake-s18 <35538542+hmc-snake-s18@users.noreply.github.com>
2019-02-03 10:51:27 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1lF_Mvm6ou0I9AD9NGgT_UU8NNXQOM75t'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Gbi-QX9RoS92wgvFqIye43Bd_cEO9quF'
 export_file_name = 'export.pkl'
 
-classes = ['Donkey', 'Horse', 'Zebra']
+classes = ['bats', 'cats', 'hats']
 path = Path(__file__).parent
 
 app = Starlette()
